a1ae5b290f7517239824be300f6d62b89c276d82
Toby Such <toby@tobysuch.uk>
2020-02-01 16:08:06 +0000

    Remove database and docker

--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -75,10 +75,6 @@ WSGI_APPLICATION = 'webserver.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
